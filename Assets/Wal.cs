6d707dc96c532f8ac8b02908f9f832a6c6f0e03e
ilBuso <alessandro.busola@gmail.com>
2022-01-29 21:08:00 +0100

    29/01/2022
    Basics

---
+++ Assets/Wal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("dentro");
+    }
+}
